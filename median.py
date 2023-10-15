b0dc1d1fad7882cbb0e4508d449ec4bd0e5fa1c9
Rey Almubarak <raghad.almub@gmail.com>
2023-10-15 17:03:26 +0100

    writes a solution for median excerise

--- median.py
+++ median.py
@@ -5,8 +5,14 @@ while True:
     try:
         print("Enter a list of numbers separated by commas: ")
         numbers = [float(value) for value in input().split(",")]
+        numbers.sort()
+
+        if len(numbers) % 2 == 0:
+            print(result = (numbers[len(numbers)//2-1] + numbers[len(numbers)//2]) / 2)
+        else: 
+            print(numbers[len(numbers)//2])
+
     except ValueError:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
